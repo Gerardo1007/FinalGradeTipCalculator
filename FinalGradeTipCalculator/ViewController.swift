6c68f0820f7c5a4d68ce2b06637a0aae040f2fcd
GERARDO DEHUMA <rl200066@rlas-macair-23.local>
2018-12-13 10:51:43 -0600

    updated app

--- FinalGradeTipCalculator/ViewController.swift
+++ FinalGradeTipCalculator/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var theCostOfTheMealTextField: UITextField!
+    @IBOutlet weak var tipPercentageTextField: UITextField!
+    @IBOutlet weak var theTipYouLeavingLabel: UILabel!
+    @IBOutlet weak var theCostOfTheMealLabel: UILabel!
+    @IBOutlet weak var totalCostPlusTipLabel: UILabel!
+    
+    var tipPercentage = Double()
+    var bill = Double()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func calculateEverything(_ sender: Any) {
+        tipPercentage = Double(tipPercentageTextField.text!) ?? 0
+        bill = Double(theCostOfTheMealLabel.text!) ?? 0
+        let tipDecimal = tipPercentage / 100
+        let tipYouWantToLeave = tipDecimal * bill
+        let totalCostPlusTheTip = tipYouWantToLeave + bill
+        theTipYouLeavingLabel.text = "the tip you are leaving:\(tipYouWantToLeave)"
+        print(tipYouWantToLeave)
+        
+//      totalPayLabel.text = "Total Pay = $\(totalPay)"
+        
+        //    let number1 = Double(textField1.text!)
+        
+    }
+    
 }
 
